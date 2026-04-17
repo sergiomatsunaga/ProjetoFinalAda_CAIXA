dbaa909e139d72f5e7eca55a5f0ce5b45c77f571
Sergio Matsunaga <sergiomatsunaga@gmail.com>
2026-04-17 12:03:28 -0300

    primeiro commit

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+movies = []
+
+@app.route('/')
+def home():
+    return "MovieFlix API rodando!"
+
+@app.route('/movies', methods=['POST'])
+def add_movie():
+    data = request.json
+    movies.append(data)
+    return jsonify(data)
+
+@app.route('/movies', methods=['GET'])
+def get_movies():
+    return jsonify(movies)
+
+app.run(host='0.0.0.0', port=5000)
